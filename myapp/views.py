83d2701e876fbd08a3f53449d615ffa25ecd62a7
gre888 <gregary8@gmail.com>
2026-08-07 08:30:05 +0800

    update0807

--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,18 @@ def sayhello(request):
 def hello1(request,username):
     print(username)
     return HttpResponse(f"<b>Hello {username}!!!!!</b>")
+
+from datetime import datetime
+def hello2(request,username):
+    print(f"username: {username}")
+    now = datetime.now() #產生現在時間
+    print(f"now: {now}") #列印現在時間
+    # return HttpResponse("Hello2")
+    return render(request, "hello2.html", locals())
+  
+def hello3(request,username):
+    print(f"username: {username}")
+    now = datetime.now() #產生現在時間
+    print(f"now: {now}") #列印現在時間
+    #return HttpResponse("Hello3")
+    return render(request, "hello3.html", locals())
